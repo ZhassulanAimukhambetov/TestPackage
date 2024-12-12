9c8b8cae179bc657eca5ab21a1cf4a3e4d777940
Zhassulan Aimukhambetov <aimzhas@gmail.com>
2024-12-12 18:53:34 +0500

    Release

--- Sources/TestPackage/TestPackage.swift
+++ Sources/TestPackage/TestPackage.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import UIKit
+
+public extension UIViewController {
+    func test() {
+        print("From TestPackage")
+    }
+}
